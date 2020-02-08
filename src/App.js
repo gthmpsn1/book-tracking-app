917862a5932fe6b873b784235b6c6e0bcd046e20
gthmpsn1 <gthmpsn1@gmail.com>
2020-02-07 18:17:53 -0800

    revert search

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
     books: [],
     searchResult: []
   }
+
   componentDidMount() {
     BooksAPI.getAll()
       .then((books) => {
@@ -18,7 +19,8 @@ class App extends Component {
       }))
     })
   }
- searchAPI = (query) => {
+
+  searchAPI = (query) => {
    BooksAPI.search(query)
     .then(books => {
       if (Array.isArray(books)) {
@@ -31,6 +33,7 @@ class App extends Component {
     // }))
     // } 
   })}
+
   updateShelf = (bookIndex, shelfUpdate) => {
     BooksAPI.update(this.state.books[bookIndex], shelfUpdate) 
     this.setState(prevState => {
@@ -38,6 +41,7 @@ class App extends Component {
       return {books: prevState.books}
     });
   }
+
   updateSearchShelf = (bookIndex, shelfUpdate) => {
     BooksAPI.update(this.state.searchResult[bookIndex], shelfUpdate)
     this.setState(prevState => {
@@ -45,14 +49,16 @@ class App extends Component {
       return {searchResult: prevState.searchResult}
     });
 
-    this.state.books.id.includes(this.state.searchResult[bookIndex].id) &&
-      BooksAPI.update(this.state.books[bookIndex], shelfUpdate) 
-      this.setState(prevState => {
-        prevState.books[bookIndex].shelf= shelfUpdate
-        return {books: prevState.books}
-      });
+    //only update the shelf state of selected book in 'books'
+    // this.state.books.id.includes(this.state.searchResult[bookIndex].id) &&
+    //   BooksAPI.update(this.state.books[bookIndex], shelfUpdate) 
+    //   this.setState(prevState => {
+    //     prevState.books[bookIndex].shelf= shelfUpdate
+    //     return {books: prevState.books}
+    //   });
     
-    !this.state.books.id.includes(this.state.searchResult[bookIndex].id) &&
+    // //update 'books' with this new book object to included the shelf state
+    // !this.state.books.id.includes(this.state.searchResult[bookIndex].id) &&
     BooksAPI.update(this.state.books[bookIndex], shelfUpdate) 
     this.setState(prevState => {
       prevState.books.push(this.state.searchResult[bookIndex])
@@ -65,7 +71,7 @@ class App extends Component {
       <div>
         <header className='App-header'>Gabriel's Favorite Books</header>
         <Route exact path='/' render={() => (<MyBooks books={this.state.books} updateShelf={this.updateShelf}/>)} />
-        <Route path='/search' render={() => (<SearchBooks books={this.state.books} updateShelf={this.updateShelf} searchResult={this.state.searchResult} updateSearchShelf={this.updateSearchShelf} searchAPI={this.searchAPI} />)} />
+        <Route path='/search' render={() => (<SearchBooks searchResult={this.state.searchResult} updateSearchShelf={this.updateSearchShelf} searchAPI={this.searchAPI} />)} />
       </div>
       )
   }
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -14,7 +14,7 @@ class SearchBooks extends Component {
     }
 
     render(){
-        const {searchResult, books} = this.props
+        const {searchResult} = this.props
         return(
             <div>
                 <Link to='/'>{`<<< Back`}</Link>
@@ -58,106 +58,7 @@ class SearchBooks extends Component {
 
                 </div>
 
-                <div>
-                    <h1 className='shelfHeader'>Currently Reading</h1>
-                    <div className='shelfBG1'>
-                        <div className='bookShelf'>
-                            
-                            {books.map((book, index) => (
-                                book.shelf === 'currentlyReading' &&
-                                    <div className='bookProfile' key={index}>
-                                        <img className='bookImage' src={book.imageLinks.smallThumbnail} alt={book.title}></img>
-                                        <div className='bookInfo'>
-                                            <p className='bookTitle'><b>{book.title}</b><br></br><em>{book.subtitle}</em></p>
-                                            <ul>
-                                                <li>Authors: {book.authors}</li>
-                                                <li>Published: {book.publishedDate}</li>
-                                                <li>ISBN 13: {book.industryIdentifiers[0].identifier}</li>
-                                                {/* <li>ISBN 10: {book.industryIdentifiers[1].identifier}</li> */}
-                                                <li>Pages: {book.pageCount}</li>
-                                                <li><a href={book.previewLink}>PREVIEW</a></li>
-                                            </ul>
-                                            <select
-                                                name={index}
-                                                defaultValue={book.shelf}
-                                                onChange={this.handleSearch}>
-                                                <option vaule="none">Move to...</option>
-                                                <option value="currentlyReading">Currently Reading</option>
-                                                <option value="wantToRead">Want to Read</option>
-                                                <option value="read" >Already Read</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                            ))}
-                        </div>
-                    </div>
-
-                    <h1 className='shelfHeader'>Wanting to Read</h1>
-                    <div className='shelfBG2'>
-                        <div className='bookShelf'>
-                            {books.map((book, index) => (
-                                book.shelf === 'wantToRead' &&
-                                    <div className='bookProfile' key={index}>
-                                        <img className='bookImage' src={book.imageLinks.smallThumbnail} alt={book.title}></img>
-                                        <div className='bookInfo'>
-                                            <p className='bookTitle'><b>{book.title}</b><br></br><em>{book.subtitle}</em></p>
-                                            <ul>
-                                                <li>Authors: {book.authors}</li>
-                                                <li>Published: {book.publishedDate}</li>
-                                                <li>ISBN 13: {book.industryIdentifiers[0].identifier}</li>
-                                                {/* <li>ISBN 10: {book.industryIdentifiers[1].identifier}</li> */}
-                                                <li>Pages: {book.pageCount}</li>
-                                                <li><a href={book.previewLink}>PREVIEW</a></li>
-                                            </ul>
-                                            <select
-                                                name={index}
-                                                defaultValue={book.shelf}
-                                                onChange={this.handleSearch}>
-                                                <option >Move to...</option>
-                                                <option value="currentlyReading">Currently Reading</option>
-                                                <option value="wantToRead">Want to Read</option>
-                                                <option value="read" >Already Read</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                            ))}
-                        </div>
-                    </div>
-
-                    <h1 className='shelfHeader'>Done Reading</h1>
-                    <div className='shelfBG3'>
-                        <div className='bookShelf'>
-                            {books.map((book, index) => (
-                                book.shelf === 'read' &&
-                                    <div className='bookProfile' key={index}>
-                                        <img className='bookImage' src={book.imageLinks.smallThumbnail} alt={book.title}></img>
-                                        <div className='bookInfo'>
-                                            <p className='bookTitle'><b>{book.title}</b><br></br><em>{book.subtitle}</em></p>
-                                            <ul>
-                                                <li>Authors: {book.authors}</li>
-                                                <li>Published: {book.publishedDate}</li>
-                                                <li>ISBN 13: {book.industryIdentifiers[0].identifier}</li>
-                                                {/* <li>ISBN 10: {book.industryIdentifiers[1].identifier}</li> */}
-                                                <li>Pages: {book.pageCount}</li>
-                                                <li><a href={book.previewLink}>PREVIEW</a></li>
-                                            </ul>
-                                            <select
-                                                name={index}
-                                                defaultValue={book.shelf}
-                                                onChange={this.handleSearch}>
-                                                <option vaule="none">Move to...</option>
-                                                <option value="currentlyReading">Currently Reading</option>
-                                                <option value="wantToRead">Want to Read</option>
-                                                <option value="read" >Already Read</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                            ))}
-                        </div>
-                    </div>
-
-                </div>
-
+                
 
             </div>
         )
